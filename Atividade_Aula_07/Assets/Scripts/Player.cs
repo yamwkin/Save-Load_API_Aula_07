7fc535c9c457e9736cbff21f757df024bd66c4d1
Yam Mike <mikkyamn@gmail.com>
2025-11-07 22:05:47 -0300

    Passar pro computador

---
+++ Atividade_Aula_07/Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+    public class Player
+    {
+        public int Id { get; set; }
+        public int Vida { get; set; }
+        public int QuantidadeDeItens { get; set; }
+        public int PosicaoX { get; set; }
+        public int PosicaoY { get; set; }
+        public int PosicaoZ { get; set; }
+
+    }
+
